4e44d9a877e66a2ad30058b4e4d84c21b9806bfb
patricelachelle <79610155+patricelachelle@users.noreply.github.com>
2021-08-13 14:42:32 -0500

    Edit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
-import React from 'react';
+import axios from 'axios'
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Character from './components/Character'
+import styled from 'styled-components'
+
+
+const Title = styled.h1`
+  font-size: 3.5em;
+  text-align: center;
+  color: rebeccapurple;
+`;
 
 const App = () => {
+  const [characters, setCharacters] = useState([]);
+
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
-  return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
-  );
-}
+  useEffect(() => {
+    axios.get(`https://swapi.dev/api/people`)
+    .then(({data}) => {
+      console.log(data)
+      setCharacters(data);
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }, [])
 
-export default App;
+      return (
+        <div className="App">
+          <Title className="Header">⚫🤖⚔️ Star Wars Characters! ⭐🔫🧑⚔️</Title>
+          <Character props = {characters} />
+        </div>
+      );
+    }
+    
+    export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,40 @@
-// Write your Character component here
+import React from 'react'
+import styled from 'styled-components'
+
+const Card = styled.div`
+  background: rebeccapurple;
+  color: black;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  font-size: 25px;
+  font-weight: bold;
+  height: 200px;
+  border: 20px solid green;
+`;
+
+const Wrapper = styled.div`
+    width: 100%;
+    height: 100%;
+`;
+
+function Character(info) {
+  const charInfo = info.props;
+
+  return (
+    <Wrapper>
+      {charInfo.map(data => {
+        return (
+         <Wrapper>
+         <Card>  
+         Name: {data.name}<br />D.O.B: {data.birth_year}<br />Films: {data.films.join(', ')}
+         </Card>
+         </Wrapper>
+          );
+        })}
+    </Wrapper>         
+  );
+}
+
+export default Character;
